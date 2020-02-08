0754c23db710c8144c6b284d75b39093077ed0d6
mingjun <40516784+mingjunlu@users.noreply.github.com>
2020-02-08 14:39:47 +0800

    Turn gtag.js into a gist and link to it

--- src/scripts/gtag.js
+++
@@ -1,5 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-// eslint-disable-next-line prefer-rest-params
-function gtag() { window.dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'UA-157952368-1');
