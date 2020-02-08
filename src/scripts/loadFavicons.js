9a6766e36fbc88bc818ffa74c779734d75dc42d1
mingjun <40516784+mingjunlu@users.noreply.github.com>
2020-02-08 16:51:39 +0800

    Add emoji favicons

---
+++ src/scripts/loadFavicons.js
@@ -0,0 +1,9 @@
+const loadFavicons = () => {
+    const linkTags = document.querySelectorAll('link[data-href]');
+    linkTags.forEach((tag) => {
+        tag.setAttribute('href', tag.dataset.href);
+        tag.removeAttribute('data-href');
+    });
+};
+
+loadFavicons();
