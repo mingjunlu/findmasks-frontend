44628439b821b0cf182d0d3ef4db16e4558894c5
mingjun <40516784+mingjunlu@users.noreply.github.com>
2020-02-12 11:57:30 +0800

    Remove loading screen after markers are placed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,13 @@ document.addEventListener('DOMContentLoaded', async () => {
     // eslint-disable-next-line no-undef
     const Leaflet = L;
     const maskMap = Leaflet.map('map', { preferCanvas: true });
+
+    // Draw the map
     initializeMap(maskMap);
+    await placeMarkers(maskMap);
 
     // Remove loading screen
     const layer = document.querySelector('.loading-layer');
     layer.addEventListener('transitionend', (event) => { event.target.remove(); });
     layer.classList.add('loading-layer--transparent');
-
-    await placeMarkers(maskMap);
 });
