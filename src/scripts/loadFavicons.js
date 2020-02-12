5b5b6c06a15b11ebd20335a683a8f0a8cc9f9c5c
mingjun <40516784+mingjunlu@users.noreply.github.com>
2020-02-12 11:59:33 +0800

    Turn loadFavicons.js into an IIFE

--- src/scripts/loadFavicons.js
+++ src/scripts/loadFavicons.js
@@ -1,9 +1,7 @@
-const loadFavicons = () => {
+(() => {
     const linkTags = document.querySelectorAll('link[data-href]');
     linkTags.forEach((tag) => {
         tag.setAttribute('href', tag.dataset.href);
         tag.removeAttribute('data-href');
     });
-};
-
-loadFavicons();
+})();
