313fa996262f0833b8aeafe72b375b08ba20dc62
mingjun <40516784+mingjunlu@users.noreply.github.com>
2020-04-08 13:03:41 +0800

    Update the last location after clicking a cluster

--- src/components/ClusterLayer/ClusterLayer.js
+++ src/components/ClusterLayer/ClusterLayer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactGA from 'react-ga';
 import { Layer } from 'react-mapbox-gl';
+import LastLocation from '../../classes/LastLocation';
 import changeCursor from '../../utilities/changeCursor';
 import clusterLayerProps from './clusterLayerProps';
 
@@ -9,11 +10,20 @@ const isProduction = (process.env.NODE_ENV === 'production');
 
 const ClusterLayer = ({ setMapCenter, setZoomLevel }) => {
     const inspectCluster = (event) => {
+        const { coordinates } = event.features[0].geometry;
+
+        // Zoom in and update mapCenter
         setZoomLevel((prevState) => {
             const higherZoomLevel = prevState + 1.5;
             return higherZoomLevel;
         });
-        setMapCenter(event.features[0].geometry.coordinates);
+        setMapCenter(coordinates);
+
+        // Update the last location
+        const newLocation = new LastLocation(coordinates);
+        newLocation.save();
+
+        // Track the click event
         if (isProduction) {
             ReactGA.event({
                 category: 'ClusterLayer',
