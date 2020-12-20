461d8e789f427dbbc572b7c01ba68909e18368c8
mingjun <40516784+mingjunlu@users.noreply.github.com>
2020-12-20 19:51:49 +0800

    Manually redirect to the custom domain

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ import './assets/stylesheets/reset.css';
 import './assets/stylesheets/index.css';
 import App from './App';
 
+// Redirect to the custom domain
+const shouldRedirect = window.location.host.includes('findmasks.netlify.app');
+if (shouldRedirect) {
+    window.location.replace(`https://www.findmasks.tw${window.location.pathname}`);
+}
+
 // Initialize Google Analytics
 const isProduction = (process.env.NODE_ENV === 'production');
 if (isProduction) {
